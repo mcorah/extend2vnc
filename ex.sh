64447c01668cae7a3235d835bdc1f58484e42521
Micah Corah <micahcorah@gmail.com>
2013-01-12 12:15:42 -0700

    added soft kill and other changes, wrote README

--- ex.sh
+++ ex.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+####################################
+#author and maintainer: Micah Corah
+#
+#feel free to use and modify this code as you wish
+#
+####################################
+
+
 #defaults
 GEOMS=('1280x720' '800x1200');
 GEOM=${GEOMS[0]}
@@ -17,15 +25,14 @@ for i in $@
 do
 	if [[ ${i:0:1} == "-" ]]
 	then
-		echo "form -*"
+		#echo "form -*"
 		case $i in
 		-f)
-			echo "force"
 			FORCE=1
 			CTXT="f"
 			;;
+		#kill statements
 		-k)
-			echo "kill"
 			KILL=1
 			CTXT="k"
 			;;
@@ -57,11 +64,13 @@ do
 			CTXT="d"
 			;;
 		-h)
+			echo "This script is located at: $0\n"
 			cat README
 			exit
 			;;
 		*)
-			echo "bad xarg: $i"
+			echo "bad xarg: $i\n"
+			echo "This script is located at: $0\n"
 			cat README
 			exit
 			;;
@@ -71,8 +80,13 @@ do
 		d)
 			DISP_NUM=$i
 			;;
+		s)
+			KILL="SOFT"
+			CTXT="none"
+			;;
 		*)
-			echo "bad arg: $i"
+			echo "bad arg: $i\n"
+			echo "This script is located at: $0\n"
 			cat README
 			exit
 			;;
@@ -86,30 +100,38 @@ do
 	GEOMLIST="$GEOMLIST -geometry $i"
 done
 
-if [[ "x$KILL" == "x1" ]]
-then
+#kill supercedes all other commands
+if [[ "x$KILL" == "x1" ]]; then
 	echo "killing"
 	pkill x2vnc
 	vncserver -kill :$DISP_NUM 
-	exit
-fi
-
-if [[ "x$SERV_PROC" == "x" || "x$FORCE" == "x1" ]]
-then
-	echo "starting"
-	if [[ "x$FORCE" == "x1" ]]
-	then
-		vncserver -kill :$DISP_NUM
-	fi
-	vncserver :$DISP_NUM -geometry $GEOM $GEOMLIST &
-else
-	echo "resize"
-	xrandr -d :$DISP_NUM -s $GEOM
-fi
-
-if [[ "x$X2VNC_PROC" == "x" || "x$PREV_LOC" != "x$LOC" || "x$FORCE" == "x1" ]]
-then
+elif [[ "x$KILL" == "xSOFT" ]]; then
+	#soft kill leaves server running, but kills access
+	#useful if you want to continue a session but do not want access from main desktop session
+	echo "executing soft kill"
 	pkill x2vnc
-	echo $LOC > .loc
-	x2vnc $LOC 127.0.0.1:$DISP_NUM -passwdfile ~/.vnc/pass &> /dev/null &
+else
+
+	#start vnc server
+	if [[ "x$SERV_PROC" == "x" || "x$FORCE" == "x1" ]]
+	then
+		if [[ "x$FORCE" == "x1" ]]
+		then
+			echo "killing vncserver"
+			vncserver -kill :$DISP_NUM > /dev/null
+		fi
+		echo "starting vncserver"
+		vncserver :$DISP_NUM -geometry $GEOM $GEOMLIST > /dev/null
+	else
+		echo "resize"
+		xrandr -d :$DISP_NUM -s $GEOM
+	fi
+	
+	#start vnc access/extension
+	if [[ "x$X2VNC_PROC" == "x" || "x$PREV_LOC" != "x$LOC" || "x$FORCE" == "x1" ]]
+	then
+		pkill x2vnc
+		echo $LOC > .loc
+		x2vnc $LOC 127.0.0.1:$DISP_NUM -passwdfile ~/.vnc/passwd > /dev/null &
+	fi
 fi
